5201cb56c67409a2d0431c45f53c3f58640ab48c
Drdz10 <58601479+Drdz10@users.noreply.github.com>
2021-09-08 20:01:52 -0500

    LoginAcabado

--- MARCO/LoginView.swift
+++ MARCO/LoginView.swift
@@ -1,28 +1,52 @@
+//
+//  LoginView2.swift
+//  MARCO
+//
+//  Created by Diego Rodriguez on 08/09/21.
+//
+
 import SwiftUI
 
-struct LoginView: View {
+struct LoginView2: View {
     
-    @State var username: String = ""
-    @State var password: String = ""
+    @State var signInSuccess = false
     
     var body: some View {
-        
-        ZStack{
+        return Group {
+            if signInSuccess {
+                HomeView()
+            }
+            else {
+                LoginFormView(signInSuccess: $signInSuccess)
+            }
+        }
+    }
+}
+
+struct LoginFormView : View {
+    
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
+    @State private var showError = false
+    
+    @Binding var signInSuccess: Bool
+    
+    var body: some View {
+        ZStack {
             Image("LoginBackground")
                 .resizable()
                 .ignoresSafeArea()
-        
-            VStack{
+            VStack {
                 Image("logomarco")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 280, height: 280, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .frame(width: 280, height: 280, alignment: .center)
                     
                 Text("#MUSEODETODOS")
                     .font(.system(size: 30))
                     .foregroundColor(.white)
                     
-                
                 VStack(alignment: .center, spacing: -40){
                     TextField("Usuario",text: $username)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
@@ -32,47 +56,37 @@ struct LoginView: View {
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .frame(width: 180, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                 }.padding()
-                
-                VStack(spacing:50){
-                Button(action:{
-                    print("Hello World")
-                })
-                {
+                VStack(spacing: 40){
+                Button(action: {
+                    if(self.username == self.password) {
+                        self.signInSuccess = true
+                    }
+                    else {
+                        self.showError = true
+                    }
+                    
+                }) {
                     Text("Ingresar")
                         .frame(width: 200 , height: 50, alignment: .center)
-
                 }
-                    .foregroundColor(.white)
-                    .background(Color("BotonIngresar"))
-                    .frame(width: 100)
+                .foregroundColor(.white)
+                .background(Color("BotonIngresar"))
+                .frame(width: 100)
                 
-                    
                 
-
-            
-                    VStack(spacing:0){
-                Text("¿No tienes cuenta?")
-                    .foregroundColor(.white)
-                Text("Registrate Aqui")
-                    .foregroundColor(.blue)
-                    }
-                
+                if showError {
+                    Text("Usuario/contraseña incorrecta").foregroundColor(Color.red)
+                }
+                VStack(spacing: 0){
+            Text("¿No tienes cuenta?")
+                .foregroundColor(.white)
+            Text("Registrate Aqui")
+                .foregroundColor(.blue)
+                }
                 Text("Continuar como invitado")
                     .foregroundColor(.white)
-                
                 }
             }
         }
-        
-     
-    
     }
-    
-
-
-struct LoginView_Previews: PreviewProvider {
-    static var previews: some View {
-        LoginView()
-    }
-}
 }
