f3cfecc5cafd79d3a15708ee43ea0b26141d59b7
AgustinBlanco30 <agustinblanco333@gmail.com>
2021-10-12 20:31:34 -0500

    AudioGuias

---
+++ MARCO/Views/AudioGuiaViews/AudioGuia.swift
@@ -0,0 +1,20 @@
+//
+//  AudioGuia.swift
+//  MARCO
+//
+//  Created by Agustin Blanco on 12/10/21.
+//
+
+import SwiftUI
+
+struct AudioGuia: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AudioGuia_Previews: PreviewProvider {
+    static var previews: some View {
+        AudioGuia()
+    }
+}
